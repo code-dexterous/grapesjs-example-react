b1dc7a10d7ed99971d797bd4f4054db29d56ecee
Vijay Shukla <vshukla684@gmail.com>
2021-02-20 15:34:49 +0530

    setup grapesjs with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import grapesjs from "grapesjs";
+import gjsPresetWebage from "grapesjs-preset-webpage";
+import "./styles/main.scss";
 
 function App() {
+  const [editor, setEditor] = useState(null);
+
+  useEffect(() => {
+    const editor = grapesjs.init({
+      container: "#editor",
+      plugins: [gjsPresetWebage],
+      pluginsOpts: {
+        gjsPresetWebage: {},
+      },
+    });
+    setEditor(editor);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="editor"></div>
     </div>
   );
 }
