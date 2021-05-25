513ea28fcff7c00b282926f505cb28e256a61b75
Vijay Shukla <vshukla684@gmail.com>
2021-05-25 09:46:30 +0530

    create form and get data

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div>Landing Page</div>;
+  const [name, setName] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const handleSubmit = () => {
+    console.log("check validation");
+    if (!name) {
+      setIsValid(false);
+      return;
+    }
+    console.log("Valid submit it", name);
+  };
+
+  return (
+    <div classNameName="container">
+      <div className="row">
+        <div classNameName="col-12 mt-5">
+          <form id="create-page">
+            <div className="modal-header">
+              <h5 className="modal-title" id="addPageModalLabel">
+                Create Page
+              </h5>
+            </div>
+            <div className="modal-body">
+              <div className="col-auto">
+                <label for="name" className="form-label">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className={`form-control form-control-sm ${
+                    isValid ? "" : "is-invalid"
+                  }`}
+                  id="name"
+                  name="name"
+                  placeholder="Name of Page"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                {!isValid && (
+                  <div className="invalid-feedback">
+                    Please provide a valid name.
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary btn-sm">
+                Clear
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary btn-sm"
+                onClick={handleSubmit}
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+        <div class="col-12 my-2">
+          <table class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Slug</td>
+                <td>Action</td>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "grapesjs/dist/css/grapes.min.css";
+import "bootstrap";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
