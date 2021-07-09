12576e9d8ab9337ee9c577e4760d4d088209b79e
Vijay Shukla <vshukla684@gmail.com>
2021-07-09 10:04:43 +0530

    customize UI

--- src/Editor.js
+++ src/Editor.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import grapesjs from "grapesjs";
-import gjsPresetWebage from "grapesjs-preset-webpage";
+import gjsBlockBasic from "grapesjs-blocks-basic";
 import { useParams } from "react-router-dom";
 import "./styles/main.scss";
+import { API_HOST } from "./api_utils";
 
 const Editor = () => {
   const [editor, setEditor] = useState(null);
@@ -13,16 +14,234 @@ const Editor = () => {
   useEffect(() => {
     const editor = grapesjs.init({
       container: "#editor",
-      plugins: [gjsPresetWebage],
-      pluginsOpts: {
-        gjsPresetWebage: {},
+      blockManager: {
+        appendTo: "#blocks",
       },
+      styleManager: {
+        appendTo: "#styles-container",
+        sectors: [
+          {
+            name: "Dimension",
+            open: false,
+            buildProps: ["width", "min-height", "padding"],
+            properties: [
+              {
+                type: "integer",
+                name: "The width",
+                property: "width",
+                units: ["px", "%"],
+                defaults: "auto",
+                min: 0,
+              },
+            ],
+          },
+        ],
+      },
+      layerManager: {
+        appendTo: "#layers-container",
+      },
+      traitManager: {
+        appendTo: "#trait-container",
+      },
+      selectorManager: {
+        appendTo: "#styles-container",
+      },
+      panels: {
+        defaults: [
+          {
+            id: "basic-actions",
+            el: ".panel__basic-actions",
+            buttons: [
+              {
+                id: "visibility",
+                active: true, // active by default
+                className: "btn-toggle-borders",
+                label: '<i class="fa fa-clone"></i>',
+                command: "sw-visibility", // Built-in command
+              },
+            ],
+          },
+          {
+            id: "panel-devices",
+            el: ".panel__devices",
+            buttons: [
+              {
+                id: "device-desktop",
+                label: '<i class="fa fa-television"></i>',
+                command: "set-device-desktop",
+                active: true,
+                togglable: false,
+              },
+              {
+                id: "device-mobile",
+                label: '<i class="fa fa-mobile"></i>',
+                command: "set-device-mobile",
+                togglable: false,
+              },
+            ],
+          },
+        ],
+      },
+      deviceManager: {
+        devices: [
+          {
+            name: "Desktop",
+            width: "",
+          },
+          {
+            name: "Mobile",
+            width: "320px",
+            widthMedia: "480px",
+          },
+        ],
+      },
+      storageManager: {
+        type: "remote",
+        stepsBeforeSave: 3,
+        contentTypeJson: true,
+        storeComponents: true,
+        storeStyles: true,
+        storeHtml: true,
+        storeCss: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        id: "mycustom-",
+        urlStore: `${API_HOST}/${pageId}/content`,
+        urlLoad: `${API_HOST}/${pageId}/content`,
+      },
+      plugins: [gjsBlockBasic],
+      pluginsOpts: {
+        gjsBlockBasic: {},
+      },
+    });
+
+    // Commands
+    editor.Commands.add("set-device-desktop", {
+      run: (editor) => editor.setDevice("Desktop"),
+    });
+    editor.Commands.add("set-device-mobile", {
+      run: (editor) => editor.setDevice("Mobile"),
     });
 
     setEditor(editor);
   }, []);
 
-  return <div id="editor"></div>;
+  return (
+    <>
+      {" "}
+      <div id="navbar" className="sidenav d-flex flex-column overflow-scroll">
+        <nav className="navbar navbar-light">
+          <div className="container-fluid">
+            <span className="navbar-brand mb-0 h3 logo">Code Dexterous</span>
+          </div>
+        </nav>
+        <div className="">
+          <ul className="nav nav-tabs" id="myTab" role="tablist">
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link active"
+                id="block-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#block"
+                type="button"
+                role="tab"
+                aria-controls="block"
+                aria-selected="true"
+              >
+                <i className="fa fa-cubes"></i>
+              </button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link"
+                id="layer-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#layer"
+                type="button"
+                role="tab"
+                aria-controls="layer"
+                aria-selected="false"
+              >
+                <i className="fa fa-tasks"></i>
+              </button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link"
+                id="style-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#style"
+                type="button"
+                role="tab"
+                aria-controls="style"
+                aria-selected="false"
+              >
+                <i className="fa fa-paint-brush"></i>
+              </button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link"
+                id="trait-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#trait"
+                type="button"
+                role="tab"
+                aria-controls="trait"
+                aria-selected="false"
+              >
+                <i className="fa fa-cog"></i>
+              </button>
+            </li>
+          </ul>
+          <div className="tab-content">
+            <div
+              className="tab-pane fade show active"
+              id="block"
+              role="tabpanel"
+              aria-labelledby="block-tab"
+            >
+              <div id="blocks"></div>
+            </div>
+            <div
+              className="tab-pane fade"
+              id="layer"
+              role="tabpanel"
+              aria-labelledby="layer-tab"
+            >
+              <div id="layers-container"></div>
+            </div>
+            <div
+              className="tab-pane fade"
+              id="style"
+              role="tabpanel"
+              aria-labelledby="style-tab"
+            >
+              <div id="styles-container"></div>
+            </div>
+            <div
+              className="tab-pane fade"
+              id="trait"
+              role="tabpanel"
+              aria-labelledby="trait-tab"
+            >
+              <div id="trait-container"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="main-content">
+        <nav className="navbar navbar-light">
+          <div className="container-fluid">
+            <div className="panel__devices"></div>
+            <div className="panel__basic-actions"></div>
+          </div>
+        </nav>
+        <div id="editor"></div>
+      </div>
+    </>
+  );
 };
 
 export default Editor;
